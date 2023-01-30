bded1970ebbf2d236280282532c53fd49ae84bb8
Dorian Druart <dorian.druart@gmail.com>
2023-01-30 13:51:45 +0100

    Add function to add a task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
+import { ITask } from './Interaces';
 
 const App:React.FC = () => {
 
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadLine] = useState<number>(0);
+  const [todo, setTodo] = useState<ITask[]>([]);
+
+  const handleChange = (event:ChangeEvent<HTMLInputElement>) => {
+    if(event.target.name === 'task'){
+      setTask(event.target.value);
+    }
+    else{
+      setDeadLine(Number(event.target.value));
+    }
+  }
+
+  const addTask = () => {
+    const newTask = {
+      taskName: task,
+      deadline: deadline
+    }
+    setTodo([...todo, newTask]);
+    setTask("");
+    setDeadLine(0);
+  }
 
   return (
     <div className="App">
@@ -13,7 +34,7 @@ const App:React.FC = () => {
           <input type="text" name="task" value={task} placeholder="Add a task" />
           <input type="number" name="deadline" value={deadline} placeholder="Set a deadline (in days)" />
         </div>
-        <button>Add</button>
+        <button onClick={addTask}>Add</button>
       </div>
       <div className="todoList"></div>
     </div>
