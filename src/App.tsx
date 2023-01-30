fa22e9ba07b573439146ddd825d31f9196bd0b8f
Dorian Druart <dorian.druart@gmail.com>
2023-01-30 12:30:55 +0100

    Task form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App:React.FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadLine] = useState<number>(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" name="task" value={task} placeholder="Add a task" />
+          <input type="number" name="deadline" value={deadline} placeholder="Set a deadline (in days)" />
+        </div>
+        <button>Add</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
 }
