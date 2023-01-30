b5f743780bafe15a991c50ce1b3e9b1c91405da6
Dorian Druart <dorian.druart@gmail.com>
2023-01-30 16:24:31 +0100

    Creation of Main Routing + routes

---
+++ src/Routing/Main.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import CreateTask from '../Components/CreateTask';
+import Home from '../Components/Home';
+import ShowTasks from '../Components/ShowTasks';
+
+const Main = () => {
+return (         
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/create' element={<CreateTask/>} />
+    <Route path='/list' element={<ShowTasks/>} />
+  </Routes>
+);
+}
+export default Main;
