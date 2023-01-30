80ec3259b7c5c200d3c6f45afbc9aeeba9ccd4b3
Dorian Druart <dorian.druart@gmail.com>
2023-01-30 17:20:56 +0100

    Creation of context provider in Main

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App:React.FC = () => {
   return (
     <div className="App">
       <Header></Header>
+      
       <Main></Main>
     </div>
   );
--- src/Routing/Main.tsx
+++ src/Routing/Main.tsx
@@ -1,15 +1,19 @@
+import { createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CreateTask from '../Components/CreateTask';
 import Home from '../Components/Home';
 import ShowTasks from '../Components/ShowTasks';
+import { ITask } from '../Interfaces';
 
 const Main = () => {
-return (         
-    <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/create' element={<CreateTask/>} />
-        <Route path='/list' element={<ShowTasks/>} />
-    </Routes>
-);
+    const todoList: ITask[] = [];
+    const TodoListContext = createContext<ITask[]>([]);
+    return (         
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/create' element={<TodoListContext.Provider value={todoList}><CreateTask/></TodoListContext.Provider>} />
+            <Route path='/list' element={<TodoListContext.Provider value={todoList}><ShowTasks/></TodoListContext.Provider>} />
+        </Routes>
+    );
 }
 export default Main;
