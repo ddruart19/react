d9756848996fbb77e3cd0667a7745b601ef431ff
Dorian Druart <dorian.druart@gmail.com>
2023-02-06 14:38:39 +0100

    create task

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,39 +2,51 @@ import express, { Request, Response } from "express";
 
 const app = express()
 
-const data = [{
+interface ITask {
+    id : number;
+    taskName: string;
+    completed: boolean;
+    date: string;
+}
+
+const data: ITask[] = [{
     "id": 1,
     "taskName": "Give dog a bath",
-    "deadline": 15,
     "completed": true,
     "date": "2023-02-05"
   }, {
    "id": 2,
    "taskName": "Turn off the lights",
-   "deadline": 4,
    "completed": false,
    "date": "2023-02-10"
   }, {
    "id": 3,
    "taskName": "Eat chili",
-   "deadline": 42,
    "completed": false,
    "date": "2023-02-19"
   }, {
    "id": 4,
    "taskName": "Hug mom",
-   "deadline": 2,
    "completed": true,
    "date": "2023-02-25"
   }
  ]
 
+const dataToCreate: ITask = {
+    "id": 5,
+    "taskName": "Do a REST API",
+    "completed": false,
+    "date": "2023-02-25"
+}
 
 //Fetch all
 app.get('/tasks', (req: Request, res: Response) => res.send(data))
 
 //Fetch by id
 app.get('/task/:id', (req: Request, res: Response) => res.send(data.find(t => t.id === Number(req.params.id))))
+
+//Create task
+app.post('/task', (req: Request, res: Response) => {data.push(dataToCreate); res.send("Task successfuly added")})
 
 
 export default app;
