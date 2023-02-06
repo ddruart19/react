60c20955b1012242a12c6a1cd0f31e23db107df6
Dorian Druart <dorian.druart@gmail.com>
2023-02-06 14:14:28 +0100

    fetch all

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,37 @@ import express, { Request, Response } from "express";
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World from app.ts!'))
+const data = [{
+    "id": 1,
+    "taskName": "Give dog a bath",
+    "deadline": 15,
+    "completed": true,
+    "date": "2023-02-05"
+  }, {
+   "id": 2,
+   "taskName": "Turn off the lights",
+   "deadline": 4,
+   "completed": false,
+   "date": "2023-02-10"
+  }, {
+   "id": 3,
+   "taskName": "Eat chili",
+   "deadline": 42,
+   "completed": false,
+   "date": "2023-02-19"
+  }, {
+   "id": 4,
+   "taskName": "Hug mom",
+   "deadline": 2,
+   "completed": true,
+   "date": "2023-02-25"
+  }
+ ]
+
+app.get('/', (req: Request, res: Response) => res.send('Hello World from app.ts!YYYY'))
+
+//FetchAll
+app.get('/tasks', (req: Request, res: Response) => res.send(data))
+
 
 export default app;
