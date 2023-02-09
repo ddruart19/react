3c409ab05c882c831db44a843d18271ee35a80f4
ddruart19 <dorian.druart@gmail.com>
2023-02-09 16:12:20 +0100

    Change result type in queries.ts

--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 const Pool = require('pg').Pool
-const pool = new Pool({
+const pool  = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'todoapp',
@@ -8,9 +8,16 @@ const pool = new Pool({
   port: 5432,
 })
 
+interface taskDB {
+    id: string;
+    name: string;
+    completed: boolean;
+    date: string;
+  }
+ 
 //Fetch All tasks
 const getTasks = (request : Request, response : Response) => {
-    pool.query('SELECT * FROM task', (error: Error, results: { rows: any; }) => {
+    pool.query('SELECT * FROM task', (error: Error, results: { rows: taskDB[]; }) => {
         if (error) throw error
         response.status(200).json(results.rows)
     })
@@ -18,7 +25,7 @@ const getTasks = (request : Request, response : Response) => {
 
 //Fetch task by id
 const getTaskById = (request : Request, response : Response) => {
-    pool.query('SELECT * FROM task where task.id = $1', [request.params.id], (error: Error, results: { rows: any; }) => {
+    pool.query('SELECT * FROM task where task.id = $1', [request.params.id], (error: Error, results: { rows: taskDB[]; }) => {
         if (error) throw error
         response.status(200).json(results.rows)
     })
@@ -26,7 +33,7 @@ const getTaskById = (request : Request, response : Response) => {
 
 //Create task
 const createTask = (request : Request, response : Response) => {
-    pool.query('INSERT INTO task(name, completed, date) VALUES($1, $2, $3)', [request.body.taskName, request.body.completed, request.body.date], (error: Error, results: { rows: any; }) => {
+    pool.query('INSERT INTO task(name, completed, date) VALUES($1, $2, $3)', [request.body.taskName, request.body.completed, request.body.date], (error: Error, results: { rows: taskDB[]; }) => {
         if(error) throw error
         response.status(200).send("Task successfully added")
     })
@@ -34,7 +41,7 @@ const createTask = (request : Request, response : Response) => {
 
 //Update task
 const updateTask = (request: Request, response: Response) => {
-    pool.query('UPDATE task SET name = $1, completed = $2, date = $3 WHERE id = $4;', [request.body.taskName, request.body.completed, request.body.date, request.params.id], (error: Error, results: { rows: any; }) => {
+    pool.query('UPDATE task SET name = $1, completed = $2, date = $3 WHERE id = $4;', [request.body.taskName, request.body.completed, request.body.date, request.params.id], (error: Error, results: { rows: taskDB[]; }) => {
         if(error) throw error
         response.status(200).send("Task successfully updated")
     })
@@ -42,7 +49,7 @@ const updateTask = (request: Request, response: Response) => {
 
 //Delete task
 const deleteTask = (request: Request, response: Response) => {
-    pool.query('DELETE from task where task.id = $1', [request.params.id], (error: Error, results: { rows: any; }) => {
+    pool.query('DELETE from task where task.id = $1', [request.params.id], (error: Error, results: { rows: taskDB[]; }) => {
         if(error) throw error
         response.status(200).send("Task successfully deleted")
     })
