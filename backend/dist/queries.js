b9aaec63b81aaadeddca0a5709e8e706912f02f6
ddruart19 <dorian.druart@gmail.com>
2023-02-15 16:06:48 +0100

    Change connection param of pool in queries.js

--- backend/dist/queries.js
+++ backend/dist/queries.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'todoapp',
-    password: 'admin',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PWD,
     port: 5432,
+    ssl: { rejectUnauthorized: false }
 });
 //Fetch All tasks
 const getTasks = (request, response) => {
