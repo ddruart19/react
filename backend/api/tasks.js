1e651e0fdef1e2bef86705aea060ff04513f618b
ddruart19 <dorian.druart@gmail.com>
2023-02-15 15:08:34 +0100

    Add json message when calling tasks route

--- backend/api/tasks.js
+++ backend/api/tasks.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 router.get("/", async (req,res) => {
     try{
         pool.query('SELECT * FROM task', (error, results) => {
-            res.status(200).json(results);
+            res.status(200).json({message : "Voici la liste des tâches"});
         });
     }catch (error){
         return res.status(500).send("Server error");
