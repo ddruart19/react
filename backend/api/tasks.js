c866c06537644ec2148173559c94a8a5dffc6f81
ddruart19 <dorian.druart@gmail.com>
2023-02-15 15:02:35 +0100

    return all results variable in tasks.js

--- backend/api/tasks.js
+++ backend/api/tasks.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 router.get("/", async (req,res) => {
     try{
         pool.query('SELECT * FROM task', (error, results) => {
-            res.status(200).json(results.rows);
+            res.status(200).json(results);
         });
     }catch (error){
         return res.status(500).send("Server error");
