1188c2c1247de869f56a2a44ba1a313dbba0014d
ddruart19 <dorian.druart@gmail.com>
2023-02-17 09:46:42 +0100

    Remove log of request in createTask

--- backend/dist/queries.js
+++ backend/dist/queries.js
@@ -32,7 +32,6 @@ const getTaskById = (request, response) => {
 };
 //Create task
 const createTask = (request, response) => {
-    console.log(JSON.stringify(request))
      pool.query('INSERT INTO task(name, completed, date) VALUES($1, $2, $3)', [request.body.name, request.body.completed, request.body.date], (error, results) => {
         if (error)
             throw error;
