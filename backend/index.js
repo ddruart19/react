8b9bcf1205e31f9bcbb4f8479b4ac45df98f5c9b
Dorian Druart <dorian.druart@gmail.com>
2023-02-20 12:04:28 +0100

    Modify src file in vercel.json

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const db = require('./dist/queries');
+const PORT = 3000;
+
+//Fetch all tasks
+app.get('/api/tasks', db.getTasks);
+//Fetch task by id
+app.get('/api/task/:id', db.getTaskById);
+//Create task
+app.post('/api/task', db.createTask);
+//Update task
+app.put('/api/task/:id', db.updateTask);
+//Delete task
+app.delete('/api/task/:id', db.deleteTask);
+//Validate task
+app.put('/api/task/validate/:id', db.validateTask);
+
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
