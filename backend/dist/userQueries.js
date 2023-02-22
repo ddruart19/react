b784ce270cdf59729236e3408c87cf56d262a2a3
ddruart19 <dorian.druart@gmail.com>
2023-02-22 17:23:38 +0100

    Add log of hash in create user

--- backend/dist/userQueries.js
+++ backend/dist/userQueries.js
@@ -1,10 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const bcrypt = require('bcrypt');
-const database_1 = __importDefault(require("./database"));
+// import pool from "./database";
+// interface userDBOutput {
+//     id: number;
+//     email: string;
+//     name: string;
+//     surname: string;
+//     password: string;
+// }
 // interface userDBInput {
 //     email: string;
 //     name: string;
@@ -24,12 +28,12 @@ const createUser = (request, response, next) => {
         return bcrypt.hash(body.password, salt);
     })
         .then((hash) => {
-        database_1.default.query('INSERT INTO user(email, name, surname, password) VALUES($1, $2, $3, $4)', [body.email, body.name, body.surname, hash], (error, results) => {
-            //Send error to middleware error handling function
-            if (error)
-                return next(error);
-            response.status(201).send("User successfully created");
-        });
+        // pool.query('INSERT INTO user(email, name, surname, password) VALUES($1, $2, $3, $4)', [body.email, body.name, body.surname, hash], (error: Error, results: { rows: userDBOutput[]; }) => {
+        //     //Send error to middleware error handling function
+        //     if(error) return next(error) 
+        //     response.status(201).send("User successfully created")
+        // })
+        console.log("Hash : ", hash);
     })
         .catch((err) => console.error(err.message));
 };
