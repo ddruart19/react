5af37e29b1fbfb72e074e7a6fcbc089516a0712b
ddruart19 <dorian.druart@gmail.com>
2023-02-22 17:04:53 +0100

    Add export for pool in database.ts

--- backend/dist/database.js
+++ backend/dist/database.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.pool = void 0;
 const Pool = require('pg').Pool;
-const pool = new Pool({
+exports.pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PWD,
     port: 5432
 });
-exports.default = pool;
+exports.default = exports.pool;
--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 const Pool = require('pg').Pool
-const pool  = new Pool({
+export const pool  = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
