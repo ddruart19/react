1681022e8bcc2662db5c3ec422fbb43490c6f5db
Dorian Druart <dorian.druart@gmail.com>
2023-02-27 10:27:44 +0100

    Modify login function parameters

--- backend/dist/passport-strategy.js
+++ backend/dist/passport-strategy.js
@@ -8,10 +8,10 @@ const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const database_1 = __importDefault(require("./database"));
 const bcrypt = require('bcrypt');
-passport.use(new LocalStrategy(function verify(email, password, cb) {
+passport.use(new LocalStrategy(function verify(user, cb) {
     //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-    database_1.default.query('SELECT * FROM users WHERE email like $1', email, (error, results) => {
-        if (bcrypt.compareSync(password, results.rows[0].password)) {
+    database_1.default.query('SELECT * FROM users WHERE email like $1', user.email, (error, results) => {
+        if (bcrypt.compareSync(user.password, results.rows[0].password)) {
             return cb(null, { email: results.rows[0].email }, { message: 'Logged In Successfully' });
         }
         return cb(null, false, { message: 'Incorrect email or password.' });
--- backend/src/passport-strategy.ts
+++ backend/src/passport-strategy.ts
@@ -17,11 +17,16 @@ interface userDBOutput{
     email: string;
 }
 
+interface userLoginInput{
+    email: string;
+    password: string;
+}
+
 passport.use(new LocalStrategy(
-    function verify(email: string, password:string, cb: (error: any, user: userDBOutput | false, message: any) => any) {
+    function verify(user:userLoginInput, cb: (error: any, user: userDBOutput | false, message: any) => any) {
         //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-        pool.query('SELECT * FROM users WHERE email like $1', email,(error: Error, results: { rows: userDB[]; }) => {
-            if(bcrypt.compareSync(password, results.rows[0].password)){
+        pool.query('SELECT * FROM users WHERE email like $1', user.email,(error: Error, results: { rows: userDB[]; }) => {
+            if(bcrypt.compareSync(user.password, results.rows[0].password)){
                 return cb(null, {email: results.rows[0].email}, {message: 'Logged In Successfully'});
             }
             return cb(null, false, {message: 'Incorrect email or password.'});
