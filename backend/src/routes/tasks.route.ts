9e810b3c71d6c78210a61ef7d6b432b609a70322
Dorian Druart <dorian.druart@gmail.com>
2023-03-06 16:46:20 +0100

    Add data validation in tasks route

--- backend/src/routes/tasks.route.ts
+++ backend/src/routes/tasks.route.ts
@@ -1,11 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const { body } = require('express-validator');
+import { NextFunction, Request, Response } from "express"
+import { validationResult } from "express-validator";
 
 const tasksController = require('../controllers/tasks.controller')
 
+//Error management middleware
+const errorManagement = (req: Request, res: Response, next: NextFunction) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    return next()
+  }
 
 //Create
-router.post("/", tasksController.create)
+router.post("/", 
+    body('name').not().isEmpty().trim().escape(),
+    body('completed').not().isEmpty(),
+    body('user_id').not().isEmpty(),
+    errorManagement,
+    tasksController.create
+)
 
 //Read
 router.get("/", tasksController.get)
