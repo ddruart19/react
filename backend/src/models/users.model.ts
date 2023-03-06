dd5437153a220c2914d2ce002a6402262862ef27
Dorian Druart <dorian.druart@gmail.com>
2023-03-06 15:08:56 +0100

    Add UserOutputConnection in user model

--- backend/src/models/users.model.ts
+++ backend/src/models/users.model.ts
@@ -1,19 +1,25 @@
 export interface UserInputCreate {
-    email: string;
-    name: string;
-    surname: string;
-    password: string;
-}
-
-export interface UserInputConnection {
-    email: string,
+    email: string
+    name: string
+    surname: string
     password: string
 }
 
+export interface UserInputConnection {
+    email: string
+    password: string
+}
+
+export interface UserOutputConnection {
+    email: string
+    password: string
+
+}
+
 export interface UserDB {
-    id: number;
-    email: string;
-    name: string;
-    surname: string;
-    password: string;
+    id: number
+    email: string
+    name: string
+    surname: string
+    password: string
 }
