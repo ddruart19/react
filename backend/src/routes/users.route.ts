b7c0543ae75a5482afd560c50d55c8e4f3e935b2
Dorian Druart <dorian.druart@gmail.com>
2023-03-06 16:46:36 +0100

    Add data validation in users route

--- backend/src/routes/users.route.ts
+++ backend/src/routes/users.route.ts
@@ -1,21 +1,20 @@
 import passport from "passport"
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 const express = require('express')
 const router = express.Router()
-
 import { Strategy as LocalStrategy } from 'passport-local'
 import { UserDB, UserOutputConnection } from "src/models/users.model"
+import { body, validationResult } from "express-validator"
 const bcrypt = require('bcrypt')
 const users = require('../services/users.service')
-
-
 const usersController = require('../controllers/users.controller')
 
-
+//Passport user serialization
 passport.serializeUser<any, any>((req, user, done) => {
     done(undefined, user);
 });
 
+//Passport user deserialization
 passport.deserializeUser((user: UserOutputConnection, done) => {
     users.getByEmail(user.email).then((userSession: boolean | Express.User | null | undefined) => done(null, userSession))
 });
@@ -40,9 +39,34 @@ passport.use(new LocalStrategy({
   }
 ));
 
+//Error management middleware
+const errorManagement = (req: Request, res: Response, next: NextFunction) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    return next()
+  }
+
 //Create
-router.post('/', usersController.create)
-router.post('/login', passport.authenticate("local"), (req: Request, res: Response) => {res.json(req.user)})
+router.post('/',
+    body('email').isEmail().normalizeEmail(),
+    body('name').not().isEmpty().trim().escape(),
+    body('surname').not().isEmpty().trim().escape(),
+    body('password').not().isEmpty().trim().escape(),
+    errorManagement,
+    usersController.create
+)
+
+//Authentication
+router.post('/login',
+    body('email').isEmail().normalizeEmail(),
+    body('password').not().isEmpty().trim().escape(),
+    errorManagement, 
+    passport.authenticate("local"), 
+    (req: Request, res: Response) => {res.json(req.user)}
+)
 
 
 
