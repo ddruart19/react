813a248f64420a3c23c307110fec22ad59fa9efd
ddruart19 <dorian.druart@gmail.com>
2023-03-09 14:50:51 +0100

    Add authentication and error management middleware

--- backend/src/routes/tasks.route.ts
+++ backend/src/routes/tasks.route.ts
@@ -1,27 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const { body } = require('express-validator');
-import { NextFunction, Request, Response } from "express"
-import { validationResult } from "express-validator";
+// const { body } = require('express-validator');
+// import { NextFunction, Request, Response } from "express"
+// import { validationResult } from "express-validator";
 
 const tasksController = require('../controllers/tasks.controller')
 
+//Check authentication middleware
+// const checkAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+//   if (req.isAuthenticated()) { return next() }
+//   res.status(401).json({message:"Not connected"})
+// }
+
 //Error management middleware
-const errorManagement = (req: Request, res: Response, next: NextFunction) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    return next()
-  }
+// const errorManagement = (req: Request, res: Response, next: NextFunction) => {
+//     // Finds the validation errors in this request and wraps them in an object with handy functions
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+//     return next()
+//   }
 
 //Create
 router.post("/", 
-    body('name').not().isEmpty().trim().escape(),
-    body('completed').not().isEmpty(),
-    body('user_id').not().isEmpty(),
-    errorManagement,
     tasksController.create
 )
 
