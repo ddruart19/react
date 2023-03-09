efee49e7e1cf923b40d62ced06dd1886886778b5
ddruart19 <dorian.druart@gmail.com>
2023-03-09 14:49:11 +0100

    Add session control in create and read task controller

--- backend/src/controllers/tasks.controller.ts
+++ backend/src/controllers/tasks.controller.ts
@@ -1,16 +1,41 @@
 import { Request, Response, NextFunction } from "express"
-import { TaskInputCreate, TaskInputUpdate } from "src/models/tasks.model"
+import { TaskDB, TaskInputCreate, TaskInputUpdate } from "src/models/tasks.model"
+import { UserDB } from "src/models/users.model"
 const tasks = require('../services/tasks.service')
+const users = require('../services/users.service')
+
+//Format sessionId string from cookie info
+const formatSessionId = (sessionId: string) => {
+    return sessionId.slice(2, 34)
+}
+
+//Get user from request
+const getUserFromRequest = async (req: Request) => {
+    if(req.cookies['connect.sid']){
+        //Get sessionId from request
+        let sessionId: string = formatSessionId(req.cookies['connect.sid'])
+        //Get user with sessionId
+        let user!: UserDB 
+        await users.getBySessionId(sessionId).then((res: UserDB) => user = res)
+        return user 
+    }
+    return undefined
+}
 
 //Create
 const create = async (req: Request, res: Response, next: NextFunction) => {
-    let task: TaskInputCreate = {
-        name: req.body.name,
-        completed: req.body.completed,
-        date: new Date(req.body.date),
-        user_id: req.body.user_id
-    }
     try{
+        //Get user from cookie session
+        let user: UserDB | undefined = await getUserFromRequest(req)
+        //Return status 400 if no session found
+        if(!user) return res.status(400).json({message : "Impossible to create task"})
+
+        let task: TaskInputCreate = {
+            name: req.body.name,
+            completed: req.body.completed,
+            date: new Date(req.body.date),
+            user_id: user!.id
+        }
         let result = await tasks.create(task)
         if(result > 0) return res.status(201).json({message : "Task successfully created"})
         else return res.status(400).json({message : "Impossible to create task"})
@@ -23,12 +48,20 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
 //Read
 const get = async (req: Request, res: Response, next: NextFunction) => {
     try{
-        let result
+        //Get user from cookie session
+        let user: UserDB | undefined = await getUserFromRequest(req)
+        //Return status 400 if no session found
+        if(!user) return res.status(400).json({message : "Cannot get tasks if not connected"})
+
+        let result: TaskDB[]
         if(req.query.id) result = await tasks.getById(req.query.id)
         else if(req.query.name) result = await tasks.getByName(req.query.name)
         else result = await tasks.getAll()
 
-        if(result) return res.status(200).json(result)
+        if(result){
+            result = result.filter(task => task.user_id == user!.id)
+            return res.status(200).json(result)
+        } 
         else return res.status(404).json({message : "No task found"})
     } catch(err: any) {
         console.error(`Error while getting tasks`, err.message)
