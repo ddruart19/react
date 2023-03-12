07a348f835e97ec988fdebd333a98828ca0a80e6
ddruart19 <dorian.druart@gmail.com>
2023-03-12 19:33:37 +0100

    Add sign in verification in user controller

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,9 +1,26 @@
 import { Request, Response, NextFunction } from "express"
-import { UserInputCreate } from "src/models/users.model"
+import { UserDB, UserInputCreate } from "src/models/users.model"
 const bcrypt = require('bcrypt')
 
 const users = require('../services/users.service')
 
+//Format sessionId string from cookie info
+const formatSessionId = (sessionId: string) => {
+    return sessionId.slice(2, 34)
+}
+
+//Get user from request
+const getUserFromRequest = async (req: Request) => {
+    if(req.cookies['connect.sid']){
+        //Get sessionId from request
+        let sessionId: string = formatSessionId(req.cookies['connect.sid'])
+        //Get user with sessionId
+        let user!: UserDB 
+        await users.getBySessionId(sessionId).then((res: UserDB) => user = res)
+        return user 
+    }
+    return undefined
+}
 
 //Create
 const create = async (req: Request, res: Response, next: NextFunction) => {
@@ -31,11 +48,26 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
 
     }catch(err: any){
         console.error(`Error while creating user`, err.message)
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
     return 
 }
 
+//Sign In verification
+const isSignedIn = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        //Get user from cookie session
+        let user: UserDB | undefined = await getUserFromRequest(req)
+        //Return status 400 if no session found
+        if(!user) return res.status(400).json({message : "Session not found"})
+        else return res.status(200).json({message : "Session found"})
+    } catch (err: any) {
+        console.error(`Error while checking if signed in`, err.message)
+        return res.status(500).send(err.message)
+    }
+}
+
 module.exports = {
-    create
+    create,
+    isSignedIn
 }
