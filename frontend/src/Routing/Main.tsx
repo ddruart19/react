8a670021cddb04dbafab2c9c2804a2ebd3a3801f
ddruart19 <dorian.druart@gmail.com>
2023-03-12 20:17:38 +0100

    Add authContext redirection for routes

--- frontend/src/Routing/Main.tsx
+++ frontend/src/Routing/Main.tsx
@@ -1,3 +1,6 @@
+import { ReactNode, useContext } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { Routes, Route } from 'react-router-dom';
 import CalendarTask from '../Components/CalendarTask';
 import CreateTask from '../Components/CreateTask';
@@ -7,21 +10,36 @@ import ShowTasks from '../Components/ShowTasks';
 import SignInForm from '../Components/SignInForm';
 import SignUpForm from '../Components/SignUpForm';
 import TimelineList from '../Components/TimelineList';
+import { isConnected } from '../functions';
+import authContext from '../Hooks/authContext';
+
+const queryClient = new QueryClient();
+
 
 
 
 const Main = () => {
+    
+    const { authenticated, setAuthenticated } = useContext(authContext);
+
+    const authorizationRedirection = (element: ReactNode) => {
+        return authenticated ? element : <SignInForm/>
+    }
+
     return (   
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/create' element={<CreateTask/>} />
-            <Route path='/list' element={<ShowTasks/>} />
-            <Route path='/edit/:id' element={<EditTask/>}/>
-            <Route path='/timeline' element={<TimelineList/>}/>
-            <Route path='/calendar' element={<CalendarTask/>}/>
-            <Route path='/signup' element={<SignUpForm/>}/>
-            <Route path='/signin' element={<SignInForm/>}/>
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+            <Routes>
+                <Route path='/' element={authorizationRedirection(<Home/>)} />
+                <Route path='/create' element={authorizationRedirection(<CreateTask/>)} />
+                <Route path='/list' element={authorizationRedirection(<ShowTasks/>)} />
+                <Route path='/edit/:id' element={authorizationRedirection(<EditTask/>)}/>
+                <Route path='/timeline' element={authorizationRedirection(<TimelineList/>)}/>
+                <Route path='/calendar' element={authorizationRedirection(<CalendarTask/>)}/>
+                <Route path='/signup' element={<SignUpForm/>}/>
+                <Route path='/signin' element={<SignInForm/>}/>
+            </Routes>
+            <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     );
 }
 export default Main;
