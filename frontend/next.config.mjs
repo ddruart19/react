3a6f70c630be3adf8ae35e7b05ed09f5541b6870
Dorian Druart <dorian.druart@gmail.com>
2023-03-20 15:25:35 +0100

    Add next config file

---
+++ frontend/next.config.mjs
@@ -0,0 +1,14 @@
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+  basePath: '/react',
+  assetPrefix: '/react'
+};
+
+export default nextConfig;
